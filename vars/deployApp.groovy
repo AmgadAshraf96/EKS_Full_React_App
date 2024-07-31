943502f6f1c3e780f2f505219cd48309c2af289c
Amgad Ashraf <amgad.ashraf96@gmail.com>
2024-07-31 18:48:46 +0000

    change the image name using sed

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -5,7 +5,9 @@ def call() {
                     cd cluster_dir
                     kubectl apply -f DB-PersistentVol.yaml
                     kubectl apply -f MongoDB-DS.yaml
+                    sed -i 's/\\${IMAGE_TAG_back}/v${version}/g' Backend-DS.yaml
                     kubectl apply -f Backend-DS.yaml
+                    sed -i 's/\\${IMAGE_TAG_front}/v${version}/g' Frontend-DS.yaml
                     kubectl apply -f Frontend-DS.yaml
                     cd ..
                     rm -rf cluster_dir
