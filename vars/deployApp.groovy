173fe7abdab6ca5c6d3b29595f9a965d2dd4a822
Amgad Ashraf <amgad.ashraf96@gmail.com>
2024-07-31 16:29:49 +0000

    creat configmap

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -3,6 +3,7 @@ def call() {
                     rm -rf cluster_dir
                     git clone -b App_Kube_cluster https://github.com/amgad96/EKS_Full_React_App.git cluster_dir
                     cd cluster_dir
+                    kubectl apply -f configmap.yaml
                     kubectl apply -f DB-PersistentVol.yaml
                     kubectl apply -f MongoDB-DS.yaml
                     kubectl apply -f Backend-DS.yaml
