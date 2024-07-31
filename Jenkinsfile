b7c1b571dcf27b13ec09da9a25d9a106de7933cb
Amgad Ashraf <amgad.ashraf96@gmail.com>
2024-07-31 01:25:53 +0000

    edit groovy file and jenkins file

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,7 +1,26 @@
 @Library('Kube_cluster') _ // Load the shared library
 pipeline {
     agent any 
+        environment {
+        AWS_REGION = 'us-east-1' // Replace with your AWS region
+        EKS_CLUSTER_NAME = 'prod_eks_cluster' // Replace with your EKS cluster name
+    }
     stages {
+        stage('Access_AWS') {
+            steps {
+                    withCredentials([usernamePassword(credentialsId: 'AWS_CREDENTIALS', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                    sh """
+                    # Configure AWS CLI
+                    aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+                    aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+                    aws configure set default.region ${AWS_REGION}
+                    
+                    # Update kubeconfig for EKS
+                    aws eks --region ${AWS_REGION} update-kubeconfig --name ${EKS_CLUSTER_NAME}
+                    """
+                }
+                }
+            }
         stage('Apply cluster yaml files') {
             steps {
                 script {
--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,18 +1,5 @@
-def call(AWS_CREDENTIALS) {
-
-        withCredentials([usernamePassword(credentialsId: 'AWS_CREDENTIALS', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-                    sh """
-                    # Configure AWS CLI
-                    aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-                    aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-                    aws configure set default.region us-east-1
-                    
-                    # Update kubeconfig for EKS
-                    #aws eks --region ${AWS_REGION} update-kubeconfig --name ${EKS_CLUSTER_NAME}
-                    
-                    aws eks --region us-east-1 update-kubeconfig --name prod_eks_cluster
-                    
-                    # Apply yaml files
+def call() {
+                    sh """ 
                     rm -rf cluster_dir
                     git clone -b App_Kube_cluster https://github.com/amgad96/EKS_Full_React_App.git cluster_dir
                     cd cluster_dir
@@ -23,7 +10,6 @@ def call(AWS_CREDENTIALS) {
                     cd ..
                     rm -rf cluster_dir
                     """
-    }
     /*stage('Clone Repository and Apply Kubernetes Manifests') {
             steps {
                 git branch: 'App_Kube_cluster', url: 'https://github.com/amgad96/Full_React_App.git'
