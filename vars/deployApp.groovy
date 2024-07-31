e597c6f948182d936420169ad10c023da6aeccef
Amgad Ashraf <amgad.ashraf96@gmail.com>
2024-07-31 00:06:48 +0000

    edit yaml files and change the apply command

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,29 +1,39 @@
-/*def call() {
-        echo "test the shared libray with post"
-        // Download the script file 
-        sh 'wget -O ./run_yamls.sh https://raw.githubusercontent.com/amgad96/Full_React_App/main/run_yamls.sh'
-	sshagent(['Master_Node_SSH_Cred']) {
-                // Copy the script to the remote server
-                sh 'scp -o StrictHostKeyChecking=no ./run_yamls.sh ubuntu@34.229.120.213:/home/ubuntu/run_yamls.sh'
-                // Execute the script on the remote server
-                sh 'ssh -o StrictHostKeyChecking=no ubuntu@34.229.120.213 "chmod +x /home/ubuntu/run_yamls.sh && /home/ubuntu/run_yamls.sh"'
-        }        
-}*/
-//Another way 
 def call() {
-    sshagent(['Master_Node_SSH_Cred']) {
-        sh """
-        ssh -o StrictHostKeyChecking=no ubuntu@54.221.50.171 <<EOF
-        rm -rf cluster_dir
-        git clone -b App_Kube_cluster https://github.com/amgad96/Full_React_App.git cluster_dir
-        cd cluster_dir
-        sudo kubectl apply -f DB-PersistentVol.yaml
-        sudo kubectl apply -f MongoDB-DS.yaml
-        sudo kubectl apply -f Backend-DS.yaml
-        sudo kubectl apply -f Frontend-DS.yaml
-        cd ..
-        rm -rf cluster_dir
-EOF
-        """
+         environment {
+        AWS_REGION = 'us-east-1' // Replace with your AWS region
+        EKS_CLUSTER_NAME = 'prod_eks_cluster' // Replace with your EKS cluster name
+        }
+        withCredentials([usernamePassword(credentialsId: 'AWS_CREDENTIALS', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                    sh """
+                    # Configure AWS CLI
+                    aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+                    aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+                    aws configure set default.region ${AWS_REGION}
+                    
+                    # Update kubeconfig for EKS
+                    aws eks --region ${AWS_REGION} update-kubeconfig --name ${EKS_CLUSTER_NAME}
+                    
+                    # Apply yaml files
+                    rm -rf cluster_dir
+                    git clone -b App_Kube_cluster https://github.com/amgad96/Full_React_App.git cluster_dir
+                    cd cluster_dir
+                    sudo kubectl apply -f DB-PersistentVol.yaml
+                    sudo kubectl apply -f MongoDB-DS.yaml
+                    sudo kubectl apply -f Backend-DS.yaml
+                    sudo kubectl apply -f Frontend-DS.yaml
+                    cd ..
+                    rm -rf cluster_dir
+                    """
     }
+    /*stage('Clone Repository and Apply Kubernetes Manifests') {
+            steps {
+                git branch: 'App_Kube_cluster', url: 'https://github.com/amgad96/Full_React_App.git'
+                
+                sh """
+                kubectl apply -f cluster_dir/DB-PersistentVol.yaml
+                kubectl apply -f cluster_dir/MongoDB-DS.yaml
+                kubectl apply -f cluster_dir/Backend-DS.yaml
+                kubectl apply -f cluster_dir/Frontend-DS.yaml
+                """
+            }*/
 }
